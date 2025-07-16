ec828f408a058f9f43bcb10064bac62e4b20c776
ismaeldan <ismael.dantas@hotmail.com>
2025-07-16 14:34:55 -0300

    Removendo o CO2 da pargina de serviços DTF

--- src/pages/Services/index.tsx
+++ src/pages/Services/index.tsx
@@ -30,7 +30,7 @@ export function Services() {
         <div id='dtf_co2' className={styles.service__item}>
           <div className={styles.service__item__content__reverse}>
             <div className={styles.line}></div>
-            <h2>DTF-CO2</h2>
+            <h2>DTF</h2>
             <p>A técnica DTF (Direct to Film) com adesivo é um método de personalização gráfica que imprime imagens em alta definição em um filme especial com tintas pigmentadas e pó adesivo termofusível. Após aquecimento, o adesivo é ativado para transferência. Seu diferencial é a aplicação em superfícies não têxteis (vidro, metal, plástico, madeira), ampliando a personalização para brindes, embalagens e decoração. A imagem transferida mantém fidelidade de cores, resistência, flexibilidade e durabilidade. É uma solução versátil, com acabamento profissional e aplicação simplificada, ideal para produções em pequena e média escala.</p>
             <div className={styles.line}></div>
           </div>
