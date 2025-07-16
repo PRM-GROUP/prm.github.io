b5c21a8eb975f0ff9d64f6591984cb2bcba73c68
ismaeldan <ismael.dantas@hotmail.com>
2025-07-16 14:14:55 -0300

    Envio do projeto

---
+++ src/pages/Services/index.tsx
@@ -0,0 +1,131 @@
+import styles from './services.module.css';
+import ImgBaixoRelevo from "../../assets/BAIXO RELEVO.jpg";
+import ImgDTF from "../../assets/DTF.jpg"
+import ImgLaser from "../../assets/LASER.jpg"
+import ImgSublimacao from "../../assets/SUBLIMAÇÃO.jpg"
+import ImgSilk from "../../assets/SILK.jpg"
+import ImgTampografia from "../../assets/TAMPOGRAFIA.jpg"
+import ImgTransfer from "../../assets/TRANSFER.jpg"
+import ImgDigital from "../../assets/DIGITAL.jpg"
+import ImgDigital360 from "../../assets/DIGITAL 360°.jpg"
+
+export function Services() {
+ 
+  return (
+    <main id='home_services' className={styles.services}>
+      <div className='container'>
+        <h1>Conheça nossos serviços</h1>
+
+        <div className={styles.service__item}>
+          <img src={ImgBaixoRelevo} alt="Imagem baixo relevo" />
+
+          <div id='baixo_relevo' className={styles.service__item__content}>
+            <div className={styles.line}></div>
+            <h2>Baixo Relevo</h2>
+            <p>A gravação em baixo relevo é um processo que cria marcas ou desenhos em uma superfície, removendo uma fina camada do material para formar um efeito de profundidade. Diferente do alto relevo, onde as formas se projetam para fora, no baixo relevo as áreas gravadas ficam abaixo do nível da superfície, criando um efeito sutil e elegante. Esse método é amplamente utilizado para personalizar produtos produzidos em couro e courino, conferindo-lhes durabilidade e sofisticação estética.</p>
+            <div className={styles.line}></div>
+          </div>
+        </div>
+
+        <div id='dtf_co2' className={styles.service__item}>
+          <div className={styles.service__item__content__reverse}>
+            <div className={styles.line}></div>
+            <h2>DTF-CO2</h2>
+            <p>A técnica DTF (Direct to Film) com adesivo é um método de personalização gráfica que imprime imagens em alta definição em um filme especial com tintas pigmentadas e pó adesivo termofusível. Após aquecimento, o adesivo é ativado para transferência. Seu diferencial é a aplicação em superfícies não têxteis (vidro, metal, plástico, madeira), ampliando a personalização para brindes, embalagens e decoração. A imagem transferida mantém fidelidade de cores, resistência, flexibilidade e durabilidade. É uma solução versátil, com acabamento profissional e aplicação simplificada, ideal para produções em pequena e média escala.</p>
+            <div className={styles.line}></div>
+          </div>
+
+          <img src={ImgDTF} alt="Imagem DTF" />
+        </div>
+
+        <div id='laser' className={styles.service__item}>
+          <img src={ImgLaser} alt="Imagem laser" />
+
+          <div className={styles.service__item__content}>
+            <div className={styles.line}></div>
+            <h2>Laser</h2>
+            <p>A gravação a laser é um processo de marcação de alta precisão que utiliza um feixe de laser para corrosão da superfície de um material, criando marcas permanentes. Essa técnica é amplamente utilizada em diversos materiais como metal, vidro e madeira, devido à sua capacidade de produzir resultados detalhados e permanentes</p>
+            <div className={styles.line}></div>
+          </div>
+        </div>
+
+        <div id='manuseio' className={styles.service__item}>
+          <div className={styles.service__item__content__reverse}>
+            <div className={styles.line}></div>
+            <h2>Manuseio</h2>
+            <p>O manuseio geral oferecido é um processo de desembalagem, conferencia, embalagem de forma que o cliente pode solicitar um manuseio especial, como montagem de kits. Se trata de um processo minucioso e focado em atender da melhor maneira a demanda da sua ação promocional.</p>
+            <div className={styles.line}></div>
+          </div>
+
+          <img src="https://placehold.co/540x500" alt="Trocar placeholder" />
+        </div>
+
+        <div id='silk_screen' className={styles.service__item}>
+          <img src={ImgSilk} alt="Imagem Silk" />
+
+          <div className={styles.service__item__content}>
+            <div className={styles.line}></div>
+            <h2>Silk Screen</h2>
+            <p>Um método tradicional e altamente eficaz. Neste processo, a tinta é pressionada através de uma tela esticada sobre o objeto, criando um design durável e de alta qualidade. Aplicação em camisetas, bonés, sacolas e uma variedade de materiais promocionais, a serigrafia é particularmente recomendada para grandes tiragens.</p>
+            <div className={styles.line}></div>
+          </div>
+        </div>
+
+        <div id='sublimacao' className={styles.service__item}>
+          <div className={styles.service__item__content__reverse}>
+            <div className={styles.line}></div>
+            <h2>Sublimação</h2>
+            <p>Se trata de um processo de transferência de gravação de uma superfície para outra, resultando em uma impressão de alta qualidade. Amplamente utilizado em canecas de procelana. A sublimação oferece uma reprodução nítida de designs complexos e detalhados.</p>
+            <div className={styles.line}></div>
+          </div>
+
+          <img src={ImgSublimacao} alt="Imagem Sublimação" />
+        </div>
+
+        <div className={styles.service__item}>
+          <img src={ImgTampografia} alt="Imagem Tampografia" />
+
+          <div id='tampografia' className={styles.service__item__content}>
+            <div className={styles.line}></div>
+            <h2>Tampografia</h2>
+            <p>A tinta é transferida de um clichê para o produto através de um tampão de silicone, resultando em detalhes nítidos mesmo em superfícies irregulares. Amplamente empregada em brindes corporativos como canetas, produtos eletrônicos e itens de escritório, a Tampografia oferece uma personalização durável e de qualidade.</p>
+            <div className={styles.line}></div>
+          </div>
+        </div>
+
+        <div id='transfer' className={styles.service__item}>
+          <div className={styles.service__item__content__reverse}>
+            <div className={styles.line}></div>
+            <h2>Transfer</h2>
+            <p>O design é impresso em um papel especial que posteriormente, é transferido para o brinde por meio de calor e pressão. Essa técnica oferece uma reprodução precisa de detalhes e cores, sendo ideal para designs variados. Ampla aplicação em camisetas, bonés, bolsas e uma variedade de produtos promocionais.</p>
+            <div className={styles.line}></div>
+          </div>
+
+          <img src={ImgTransfer} alt="Imagem Transfer" />
+        </div>
+
+        <div className={styles.service__item}>
+          <img src={ImgDigital} alt="Imagem Digital" />
+
+          <div id='digital' className={styles.service__item__content}>
+            <div className={styles.line}></div>
+            <h2>Gravação Digital UV</h2>
+            <p>A gravação digital UV é um processo de impressão direta que utiliza tintas especiais curadas por luz ultravioleta, resultando em impressões resistentes, duráveis e com alta definição em diversas superfícies. Este método permite cores vivas e efeitos visuais sofisticados, sendo ideal para tiragens curtas e médias devido à sua agilidade, personalização e baixo custo inicial, já que dispensa o uso de clichês ou matrizes.</p>
+            <div className={styles.line}></div>
+          </div>
+        </div>
+
+        <div id='digital360' className={styles.service__item}>
+          <div className={styles.service__item__content__reverse}>
+            <div className={styles.line}></div>
+            <h2>Gravação Digital UV 360</h2>
+            <p>A gravação digital UV 360 é uma evolução da impressão UV tradicional, focada na personalização completa de objetos cilíndricos ou curvos, como garrafas e canecas, em toda a sua circunferência. Ela utiliza a mesma tecnologia de cura por luz UV para garantir impressões duráveis, resistentes e com alta fidelidade de cores, mas se diferencia por sua capacidade de imprimir em 360 graus, através da rotação controlada do objeto, permitindo detalhes finos e efeitos como relevo em superfícies curvas.</p>
+            <div className={styles.line}></div>
+          </div>
+
+          <img src={ImgDigital360} alt="Imagem Digital 360" />
+        </div>
+      </div>  
+    </main> 
+  )
+}
