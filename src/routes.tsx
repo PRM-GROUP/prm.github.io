69751b034c750166ba247fcf5ce567bdd13ab6b1
Ismael <ismael.dantas@hotmail.com>
2025-07-22 11:41:00 -0300

    Adicionando arquivos para a branch update

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Services } from './pages/Services';
+import { BaixoRelevo } from "./pages/Services/ServicesBaixoRelevo";
 import { LayoutWithHeader } from './LayoutWithHeader';
 
 export function MainRoutes() {
@@ -9,6 +10,7 @@ export function MainRoutes() {
       <Route element={<LayoutWithHeader />}>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
+        <Route path="/services/baixorelevo" element={<BaixoRelevo />} />
       </Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
