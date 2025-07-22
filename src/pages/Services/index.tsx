311fbb1e07d5810cdf753b82453073f6be500ca9
edufanucchi <eduardofanucchi85@gmail.com>
2025-07-22 16:01:00 -0300

    Add dos botoes nos serviços

--- src/pages/Services/index.tsx
+++ src/pages/Services/index.tsx
@@ -26,7 +26,9 @@ export function Services() {
             <h2>Baixo Relevo</h2>
             <p>A gravação em baixo relevo é um processo que cria marcas ou desenhos em uma superfície, removendo uma fina camada do material para formar um efeito de profundidade. Diferente do alto relevo, onde as formas se projetam para fora, no baixo relevo as áreas gravadas ficam abaixo do nível da superfície, criando um efeito sutil e elegante. Esse método é amplamente utilizado para personalizar produtos produzidos em couro e courino, conferindo-lhes durabilidade e sofisticação estética.</p>
             
-            <Link className={styles.button} to="/services">Conheça mais</Link>
+            <div className={styles.centerButton}>
+              <Link className={styles.button} to="/services/baixorelevo">Conheça mais</Link>
+            </div>
             <div className={styles.line}></div>
           </div>
         </div>
@@ -37,7 +39,10 @@ export function Services() {
             <h2>DTF</h2>
             <p>A técnica DTF (Direct to Film) com adesivo é um método de personalização gráfica que imprime imagens em alta definição em um filme especial com tintas pigmentadas e pó adesivo termofusível. Após aquecimento, o adesivo é ativado para transferência. Seu diferencial é a aplicação em superfícies não têxteis (vidro, metal, plástico, madeira), ampliando a personalização para brindes, embalagens e decoração. A imagem transferida mantém fidelidade de cores, resistência, flexibilidade e durabilidade. É uma solução versátil, com acabamento profissional e aplicação simplificada, ideal para produções em pequena e média escala.</p>
 
-            <Link className={styles.button} to="/services">Conheça mais</Link>
+            <div className={styles.centerButton}>
+              <Link className={styles.button} to="/services/baixorelevo">Conheça mais</Link>
+            </div>
+
             <div className={styles.line}></div>
           </div>
 
@@ -51,6 +56,11 @@ export function Services() {
             <div className={styles.line}></div>
             <h2>Laser</h2>
             <p>A gravação a laser é um processo de marcação de alta precisão que utiliza um feixe de laser para corrosão da superfície de um material, criando marcas permanentes. Essa técnica é amplamente utilizada em diversos materiais como metal, vidro e madeira, devido à sua capacidade de produzir resultados detalhados e permanentes</p>
+
+            <div className={styles.centerButton}>
+              <Link className={styles.button} to="/services/baixorelevo">Conheça mais</Link>
+            </div>
+
             <div className={styles.line}></div>
           </div>
         </div>
@@ -60,6 +70,11 @@ export function Services() {
             <div className={styles.line}></div>
             <h2>Manuseio</h2>
             <p>O manuseio geral oferecido é um processo de desembalagem, conferencia, embalagem de forma que o cliente pode solicitar um manuseio especial, como montagem de kits. Se trata de um processo minucioso e focado em atender da melhor maneira a demanda da sua ação promocional.</p>
+
+            <div className={styles.centerButton}>
+              <Link className={styles.button} to="/services/baixorelevo">Conheça mais</Link>
+            </div>
+
             <div className={styles.line}></div>
           </div>
 
@@ -73,6 +88,11 @@ export function Services() {
             <div className={styles.line}></div>
             <h2>Silk Screen</h2>
             <p>Um método tradicional e altamente eficaz. Neste processo, a tinta é pressionada através de uma tela esticada sobre o objeto, criando um design durável e de alta qualidade. Aplicação em camisetas, bonés, sacolas e uma variedade de materiais promocionais, a serigrafia é particularmente recomendada para grandes tiragens.</p>
+
+            <div className={styles.centerButton}>
+              <Link className={styles.button} to="/services/baixorelevo">Conheça mais</Link>
+            </div>
+
             <div className={styles.line}></div>
           </div>
         </div>
@@ -82,6 +102,11 @@ export function Services() {
             <div className={styles.line}></div>
             <h2>Sublimação</h2>
             <p>Se trata de um processo de transferência de gravação de uma superfície para outra, resultando em uma impressão de alta qualidade. Amplamente utilizado em canecas de procelana. A sublimação oferece uma reprodução nítida de designs complexos e detalhados.</p>
+
+            <div className={styles.centerButton}>
+              <Link className={styles.button} to="/services/baixorelevo">Conheça mais</Link>
+            </div>
+
             <div className={styles.line}></div>
           </div>
 
@@ -95,6 +120,11 @@ export function Services() {
             <div className={styles.line}></div>
             <h2>Tampografia</h2>
             <p>A tinta é transferida de um clichê para o produto através de um tampão de silicone, resultando em detalhes nítidos mesmo em superfícies irregulares. Amplamente empregada em brindes corporativos como canetas, produtos eletrônicos e itens de escritório, a Tampografia oferece uma personalização durável e de qualidade.</p>
+
+            <div className={styles.centerButton}>
+              <Link className={styles.button} to="/services/baixorelevo">Conheça mais</Link>
+            </div>
+
             <div className={styles.line}></div>
           </div>
         </div>
@@ -104,6 +134,11 @@ export function Services() {
             <div className={styles.line}></div>
             <h2>Transfer</h2>
             <p>O design é impresso em um papel especial que posteriormente, é transferido para o brinde por meio de calor e pressão. Essa técnica oferece uma reprodução precisa de detalhes e cores, sendo ideal para designs variados. Ampla aplicação em camisetas, bonés, bolsas e uma variedade de produtos promocionais.</p>
+
+            <div className={styles.centerButton}>
+              <Link className={styles.button} to="/services/baixorelevo">Conheça mais</Link>
+            </div>
+
             <div className={styles.line}></div>
           </div>
 
@@ -117,6 +152,11 @@ export function Services() {
             <div className={styles.line}></div>
             <h2>Gravação Digital UV</h2>
             <p>A gravação digital UV é um processo de impressão direta que utiliza tintas especiais curadas por luz ultravioleta, resultando em impressões resistentes, duráveis e com alta definição em diversas superfícies. Este método permite cores vivas e efeitos visuais sofisticados, sendo ideal para tiragens curtas e médias devido à sua agilidade, personalização e baixo custo inicial, já que dispensa o uso de clichês ou matrizes.</p>
+
+            <div className={styles.centerButton}>
+              <Link className={styles.button} to="/services/baixorelevo">Conheça mais</Link>
+            </div>
+
             <div className={styles.line}></div>
           </div>
         </div>
@@ -126,6 +166,11 @@ export function Services() {
             <div className={styles.line}></div>
             <h2>Gravação Digital UV 360</h2>
             <p>A gravação digital UV 360 é uma evolução da impressão UV tradicional, focada na personalização completa de objetos cilíndricos ou curvos, como garrafas e canecas, em toda a sua circunferência. Ela utiliza a mesma tecnologia de cura por luz UV para garantir impressões duráveis, resistentes e com alta fidelidade de cores, mas se diferencia por sua capacidade de imprimir em 360 graus, através da rotação controlada do objeto, permitindo detalhes finos e efeitos como relevo em superfícies curvas.</p>
+
+            <div className={styles.centerButton}>
+              <Link className={styles.button} to="/services/baixorelevo">Conheça mais</Link>
+            </div>
+            
             <div className={styles.line}></div>
           </div>
 
