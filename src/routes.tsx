be46c78a2af76d3a63845f56ac4cff0b377224eb
Ismael <ismael.dantas@hotmail.com>
2025-07-22 12:09:53 -0300

    Adicionando pagina dtf

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { Home } from './pages/Home';
 import { Services } from './pages/Services';
 import { BaixoRelevo } from "./pages/Services/ServicesBaixoRelevo";
 import { LayoutWithHeader } from './LayoutWithHeader';
+import { DTF } from './pages/Services/ServicesDTF';
 
 export function MainRoutes() {
   return (
@@ -11,6 +12,7 @@ export function MainRoutes() {
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/services/baixorelevo" element={<BaixoRelevo />} />
+        <Route path="/services/dtf" element={<DTF />} />
       </Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
