557fb05a6cc6891e9c66ca437c8e4905ef73a00e
ismaeldan <ismael.dantas@hotmail.com>
2025-07-29 09:32:25 -0300

    Mudando arquivo vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,28 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/prm.github.io/'
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()]
-  }
-
-  if (command === 'serve') {
-    // Desenvolvimento local
-    config.base = '/'
-  } else {
-    // Build para produção (GitHub Pages)
-    config.base = '/prm.github.io/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
+  base: '/prm.github.io/'
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ command }) => {
+//   const config = {
+//     plugins: [react()]
+//   }
+
+//   if (command === 'serve') {
+//     // Desenvolvimento local
+//     config.base = '/'
+//   } else {
+//     // Build para produção (GitHub Pages)
+//     config.base = '/prm.github.io/'
+//   }
+
+//   return config
+// })
