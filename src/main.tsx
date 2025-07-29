673dbaad040750886ff62f85b46cfe74abc6461b
ismaeldan <ismael.dantas@hotmail.com>
2025-07-29 15:24:31 -0300

    Modificando o routes de browser para hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { MainRoutes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <MainRoutes />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
