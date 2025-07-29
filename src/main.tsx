e399b94ff3a757d50336a5b93c4b683dee0fa46d
ismaeldan <ismael.dantas@hotmail.com>
2025-07-29 17:04:16 -0300

    Atualizações finais

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { MainRoutes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <MainRoutes />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
