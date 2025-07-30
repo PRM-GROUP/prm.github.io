09f5cad6797cf6aafa03418c56e1a571da1a5024
ismaeldan <ismael.dantas@hotmail.com>
2025-07-30 09:38:50 -0300

    Modificando arquivo de rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { LayoutWithHeader } from './LayoutWithHeader'
 import { Home } from './pages/Home'
 import { About } from './pages/About'
@@ -17,21 +17,23 @@ import { Contato } from './pages/Contact'
 
 export function MainRoutes() {
   return (
-    <Route element={<LayoutWithHeader />}>
-      <Route path="/" element={<Home />} />
-      <Route path="/sobre" element={<About />} />
-      <Route path="/servicos" element={<Services />} />
-      <Route path="/gravacao-em-baixo-relevo" element={<BaixoRelevo />} />
-      <Route path="/dtf" element={<DTF />} />
-      <Route path="/gravacao-a-laser" element={<Laser />} />
-      <Route path="/transfer" element={<Transfer />} />
-      <Route path="/digital" element={<Digital />} />
-      <Route path="/digital360" element={<Digital360 />} />
-      <Route path="/silk" element={<Silk />} />
-      <Route path="/sublimacao" element={<Sublimacao />} />
-      <Route path="/tampografia" element={<Tampografia />} />
-      <Route path="/prw" element={<PRW />} />
-      <Route path="/fale-conosco" element={<Contato />} />
-    </Route>
+    <Routes>
+      <Route element={<LayoutWithHeader />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<About />} />
+        <Route path="/servicos" element={<Services />} />
+        <Route path="/gravacao-em-baixo-relevo" element={<BaixoRelevo />} />
+        <Route path="/dtf" element={<DTF />} />
+        <Route path="/gravacao-a-laser" element={<Laser />} />
+        <Route path="/transfer" element={<Transfer />} />
+        <Route path="/digital" element={<Digital />} />
+        <Route path="/digital360" element={<Digital360 />} />
+        <Route path="/silk" element={<Silk />} />
+        <Route path="/sublimacao" element={<Sublimacao />} />
+        <Route path="/tampografia" element={<Tampografia />} />
+        <Route path="/prw" element={<PRW />} />
+        <Route path="/fale-conosco" element={<Contato />} />
+      </Route>
+    </Routes>
   )
 }
